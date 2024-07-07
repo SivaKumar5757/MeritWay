a47c2bd216b41898556f4e82fdf092b79447016a
siva kumar <146540716+SivaKumar5757@users.noreply.github.com>
2024-07-07 22:23:53 +0530

    Change Port

--- main.py
+++ main.py
@@ -115,4 +115,4 @@ def playing(subpath):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
